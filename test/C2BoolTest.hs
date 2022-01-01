d2364b41f90b159d1d3c66d4ba7228cc4e9b269f
Prem Muthedath <prem.muthedath@gmail.com>
2022-01-01 19:38:57 +0530

    test/C2BoolTest.hs: 1. + `Sides` data type & refactored code to use it instead of a list to avoid runtime pattern match errors; 2. + test for `leap4`.

--- test/C2BoolTest.hs
+++ test/C2BoolTest.hs
@@ -46,6 +46,18 @@ prop_leap = forAll genLeap $
               \x -> leap_classifys x $
                     leap' x === True
 
+-- | property to test a leap year not divisible by 100.
+prop_leap4 :: Property
+prop_leap4 = forAll leap4'  $
+              \x -> leap_classifys x $
+                    leap' x === leap' (x + 4)
+  where leap4' :: Gen Int
+        leap4' = do
+          y :: Int <- genYear leap4   -- NOTE: y `mod` 100 /= 0
+          if (y + 4) `mod` 100 == 0
+             then return $ y - 4
+             else return y
+
 -- | property to test a non-leap year.
 prop_non_leap :: Property
 prop_non_leap = forAll (genYear notLeap) $
@@ -86,6 +98,7 @@ notLeap x = (not (leap100 x)) && (not (leap4 x))
 -- | quickcheck testing -- triangle stuff
 --------------------------------------------------------------------------------
 -- | `Triangle` data type.
+-- list of all possible traingles considered.
 data Triangle = Equilateral
                 | Isoceles
                 | Scalene
@@ -98,119 +111,131 @@ data Triangle = Equilateral
 instance Arbitrary (Triangle) where
   arbitrary = elements [toEnum 0 :: Triangle ..]
 --------------------------------------------------------------------------------
+-- | `Sides` data type: represents sides of a triangle, good or bad.
+-- `Sides x y z` represents sides `x`, `y`, `z` of a triangle in order, where 
+-- all sides are + integers > 0, and x <= y <= z.
+-- NOTE: this definition does NOT enforce the triangle criterion, `x + y < z`.
+data Sides = Sides Int Int Int deriving (Eq, Show)
+--------------------------------------------------------------------------------
+-- | `Arbitrary` instance for `Sides`.
+instance Arbitrary Sides where
+  arbitrary = do
+      xs :: [Positive Int] <- vectorOf 3 (arbitrary :: Gen (Positive Int))
+      case (sort xs) of
+        (x:y:z:[])  -> return $
+                        Sides (getPositive x)
+                              (getPositive y)
+                              (getPositive z)
+        _           -> error "orbitrary.Sides: only 3-element list allowed."
+--------------------------------------------------------------------------------
 -- | check if `Triangle` generator is valid.
 prop_validTriangle :: Property
 prop_validTriangle = forAll (arbitrary :: Gen Triangle) $
   \x -> x `elem` [toEnum 0 :: Triangle .. ]
 --------------------------------------------------------------------------------
--- | check if lists generated are sorted, have right size, and elems are > 0.
-prop_trian_validList :: Property
-prop_trian_validList = forAll (assorted :: Gen [Int]) $
-                          \xs -> classifys xs $
-                                 (sort xs === xs) .&&.
-                                 (length xs === 3) .&&.
-                                 (all (> 0) xs)
+-- | check if `Sides` generater is valid.
+prop_trian_validSides :: Property
+prop_trian_validSides = forAll (assorted :: Gen Sides) $
+                          \s@(Sides x y z) ->
+                            let xs = [x, y, z]
+                            in classifys s $
+                               (sort xs === xs) .&&. (all (> 0) xs)
 --------------------------------------------------------------------------------
 -- | for random valid inputs, check if outputs are valid.
 prop_trian_assorted :: Property
 prop_trian_assorted = forAll assorted $
-  \(a:b:c:[]) -> classifys [a, b, c] $
-                 (analyze a b c) `elem` [0 .. 3]
+  \s@(Sides a b c) -> classifys s $
+                      (analyze a b c) `elem` [0 .. 3]
 --------------------------------------------------------------------------------
 -- | for random valid inputs, outputs remain same even when inputs are doubled.
 prop_trian_double :: Property
 prop_trian_double = forAll assorted $
-  \(a:b:c:[]) -> classifys [a, b, c] $
-                 analyze a b c === analyze (2*a) (2*b) (2*c)
+  \s@(Sides a b c) -> classifys s $
+                      analyze a b c === analyze (2*a) (2*b) (2*c)
 --------------------------------------------------------------------------------
 -- | property to test outputs for non-existent triangle.
 prop_trian_bad :: Property
-prop_trian_bad = forAll (genList Bad) $
-  \(a:b:c:[]) -> classifys [a, b, c] $
-                 analyze a b c === 0
+prop_trian_bad = forAll (genSides Bad) $
+  \s@(Sides a b c) -> classifys s $
+                      analyze a b c === 0
 --------------------------------------------------------------------------------
 -- | property to test outputs for equilateral triangle.
 prop_trian_equi :: Property
-prop_trian_equi = forAll (genList Equilateral) $
-  \(a:b:c:[]) -> classifys [a, b, c] $
-                 analyze a b c === 1
+prop_trian_equi = forAll (genSides Equilateral) $
+  \s@(Sides a b c) -> classifys s $
+                      analyze a b c === 1
 --------------------------------------------------------------------------------
 -- | property to test outputs for isoceles triangle.
 prop_trian_iso :: Property
-prop_trian_iso = forAll (genList Isoceles) $
-  \(a:b:c:[]) -> classifys [a, b, c] $
-                 analyze a b c === 2
+prop_trian_iso = forAll (genSides Isoceles) $
+  \s@(Sides a b c) -> classifys s $
+                      analyze a b c === 2
 --------------------------------------------------------------------------------
 -- | property to test outputs for scalene triangle.
 prop_trian_scal :: Property
-prop_trian_scal = forAll (genList Scalene) $
-  \(a:b:c:[]) -> classifys [a, b, c] $
-                 analyze a b c === 3
+prop_trian_scal = forAll (genSides Scalene) $
+  \s@(Sides a b c) -> classifys s $
+                      analyze a b c === 3
 --------------------------------------------------------------------------------
 -- | helper functions.
 
--- | generate random 3-elem ordered list; all elements are +ve integers.
--- some lists may represent sides of a real triangle, while others not.
-assorted :: Gen [Int]
+-- | generate sides for a random `Triangle`, good or bad.
+assorted :: Gen Sides
 assorted = do
   triangle :: Triangle <- (arbitrary :: Gen Triangle)
-  genList triangle
+  genSides triangle
 
--- | generate 3-elem, sorted, random list whose elements satisfy the property 
--- associated with the supplied `Triangle`. list elements are all +ve integers.
-genList :: Triangle -> Gen [Int]
--- NOTE: for `Equilateral`, because getting a list of identical elements through 
--- random sampling is a rare event, it takes a while to generate an 
--- `Equilateral` triangle like the way other triangles are generated. so we use 
--- a different algorithm to generate an `Equilateral` triangle.
-genList Equilateral = (replicate 3) <$> chooseInt (1, 1000)
-genList triangle    = listOf3 `suchThat` f
-  where f :: ([Int] -> Bool)
+-- | generate sides for a `Triangle`.
+genSides :: Triangle -> Gen Sides
+-- NOTE: because generating `Sides` that has all identical values through random 
+-- sampling is a rare event, it takes a while to generate sides of an 
+-- `Equilateral` triangle like the way it is done for other triangles. so we use 
+-- a different algorithm for an `Equilateral` triangle.
+genSides Equilateral = do
+        x <- chooseInt (1, 1000)
+        return $ Sides x x x
+genSides triangle    = (arbitrary :: Gen Sides) `suchThat` f
+  where f :: (Sides -> Bool)
         f | triangle `elem` [Mix, Bad] = g
           | otherwise                  = \x -> g x && h x
-        g :: ([Int] -> Bool)
+        g :: (Sides -> Bool)
         g = property' triangle
-        h :: ([Int] -> Bool)
+        h :: (Sides -> Bool)
         h = property' Good
-        -- | sorted random list of 3 elements. all elements are +ve integers.
-        listOf3 :: Gen [Int]
-        listOf3 = do
-            xs :: [Positive Int] <- sort <$> vectorOf 3 (arbitrary :: Gen (Positive Int))
-            return $ map getPositive xs
 
--- return property (a lambda) associated with supplied `Triangle`.
+-- return property (a lambda) associated with a given `Triangle`.
+-- `property'`represents rules the sides of a given `Triangle` must obey.
 -- NOTE: in case of valid triangles other than `Good`, the returned property 
 -- does NOT include the basic check of whether or not something is a triangle.  
--- if you need to generate a valid triangle, use `genList` given above.
--- NOTE: the properties (i.e., lamdas) ASSUME that the `list` is SORTED.
-property' :: Triangle -> ([Int] -> Bool)
+-- if you need to generate a valid triangle, use `genSides` given above.
+property' :: Triangle -> (Sides -> Bool)
 -- we define somewhat complex & ingenious triangle properties, because we do not 
 -- want to replicate the source code here, and in that way duplicate bugs!
-property' Equilateral = \xs@(x:_:_:[]) -> sum xs == 3 * x
-property' Isoceles    = \xs@(x:y:z:[]) ->
-                          (sum xs /= 3 * x) &&
-                          ((sum xs == 2 * y + z) ||
-                           (sum xs == x + 2 * y))
-property' Scalene     = \xs@(x:y:z:[]) ->
-                          (sum xs > 2 * x + z) &&
-                          (sum xs < 2 * y + z) &&
-                          (sum xs > x + 2 * y) &&
-                          (sum xs < 3 * z)
-property' Good        = \(x:y:z:[]) -> x + y > z
-property' Bad         = \(x:y:z:[]) -> x + y <= z
-property' Mix         = \(x:_:z:[]) -> x /= z
+property' Equilateral = \(Sides x y z) -> x + y + z == 3 * x
+property' Isoceles    = \(Sides x y z) ->
+                          let tot = x + y + z
+                          in (tot /= 3 * x) &&
+                             ((tot == 2 * y + z) ||
+                              (tot == x + 2 * y))
+property' Scalene     = \(Sides x y z) ->
+                          let tot = x + y + z
+                          in (tot > 2 * x + z) &&
+                             (tot < 2 * y + z) &&
+                             (tot > x + 2 * y) &&
+                             (tot < 3 * z)
+property' Good        = \(Sides x y z) -> x + y > z
+property' Bad         = \(Sides x y z) -> x + y <= z
+property' Mix         = \(Sides x _ z) -> x /= z
 
 -- | classifications for a property used in testing triangle construction.
-classifys :: (Testable prop) => [Int] -> prop -> Property
-classifys xs@(_:_:_:[]) = classify (length xs == 3) "3-elem list input" .
-                          classify (property' Bad $ xs)  "bad" .
-                          if (property' Good $ xs)
-                            then classify (property' Good $ xs) "triangle" .
-                                 classify (property' Equilateral $ xs) "equilateral" .
-                                 classify (property' Isoceles $ xs) "isoceles" .
-                                 classify (property' Scalene $ xs) "scalene"
-                            else classify (property' Good $ xs) "triangle"
-classifys _             = error "need exactly a 3-element list."
+classifys :: (Testable prop) => Sides -> prop -> Property
+classifys s = classify (property' Bad $ s)  "bad" .
+              if (property' Good $ s)
+                 then classify (property' Good $ s) "triangle" .
+                      classify (property' Equilateral $ s) "equilateral" .
+                      classify (property' Isoceles $ s) "isoceles" .
+                      classify (property' Scalene $ s) "scalene"
+                 else classify (property' Good $ s) "triangle"
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
