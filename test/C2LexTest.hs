e54d1246a7473303e1889638cad777eac4a83f5d
Prem Muthedath <prem.muthedath@gmail.com>
2022-01-15 23:22:39 +0530

    test/C2LexTest.hs: edited comment.

--- test/C2LexTest.hs
+++ test/C2LexTest.hs
@@ -124,8 +124,8 @@ genFractional = do
 -- | generate 'bad' string that results in parse failure (i.e., []).
 -- NOTE: to generate string that will result in parse failure, we eliminate all 
 -- cases we know will result in good parse. this step by itself will go a long 
--- way, but it is incomplete. so we also ensure generation of 2 known patterns 
--- that result in parse failure:
+-- way, but it is not enough. so in addition to elimination of all good parses, 
+-- we ensure generation of 2 known patterns that result in parse failure:
 --    a) a string that starts with 2 single quotes enclosing a single quote;
 --    b) a string that begins with a double quote but has none further on.
 --
