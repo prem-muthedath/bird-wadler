41d6e146bedc592fc1fb5bd24d4ffd875410d18e
Prem Muthedath <prem.muthedath@gmail.com>
2022-01-27 19:54:47 +0530

    test/C2BinaryTest.hs: a) + generator, properties for `bad Int string`; b) factored out common functions; c) + `classify` to properties; d) +, edited comments.

--- test/C2BinaryTest.hs
+++ test/C2BinaryTest.hs
@@ -27,6 +27,33 @@ import Common (ghciRunner)
 --------------------------------------------------------------------------------
 -- | quickcheck testing -- binary stuff.
 --------------------------------------------------------------------------------
+-- | common functions.
+--------------------------------------------------------------------------------
+-- | allowed `Int` upper bound.
+upperInt :: Int
+upperInt = (2 :: Int) ^ (32 :: Int) - 1
+
+-- | convert string -> `Int`.
+asInt :: String -> Int
+asInt = read
+
+-- | `True` if string represents a valid number, positive or negative.
+isNum :: String -> Bool
+isNum s = case s of
+            ""          -> False
+            ('0':[])    -> True
+            ('0':_)     -> False
+            ('-':[])    -> False
+            ('-':'0':_) -> False
+            ('-':xs)    -> f xs
+            _           -> f s
+  where f :: String -> Bool
+        f = all (`elem` "0123456789")
+
+-- | `True` if string does not represent a valid number.
+notNum :: String -> Bool
+notNum = not . isNum
+--------------------------------------------------------------------------------
 -- | generators.
 --------------------------------------------------------------------------------
 -- | generate `Int` string, with generated values in range 0 .. 4294967295.
@@ -44,13 +71,25 @@ import Common (ghciRunner)
 --      where digits :: String = "0123456789"
 genIntStr :: Gen String
 genIntStr = do
-  -- NOTE: 4294967295 = 2 ^ (32 :: Int) - 1
-  num1 :: Int <- elements [0, 4294967295]
+  num1 :: Int <- elements [0, upperInt]
   num2 :: Int <- (arbitrary :: Gen Int)
                  `suchThat`
-                 (\x -> x > 0 && x < 4294967295)
+                 (\x -> x > 0 && x < upperInt)
   frequency [ (1, return $ show num1)
             , (20, return $ show num2)
+            ]
+--------------------------------------------------------------------------------
+-- | generate "bad" `Int` string.
+genBadIntStr :: Gen String
+genBadIntStr = do
+  x1 :: String <- listOf $ (arbitrary :: Gen Char)
+                           `suchThat`
+                           (not . (`elem` "0123456789"))
+  x2 :: String <- show <$> ((arbitrary :: Gen Int)
+                            `suchThat`
+                            (\x -> x < 0 || x > upperInt))
+  frequency [ (1, return x1)
+            , (1, return x2)
             ]
 --------------------------------------------------------------------------------
 -- | test generators!
@@ -58,17 +97,30 @@ genIntStr = do
 -- | check if generated `Int` string is valid.
 prop_validIntStr :: Property
 prop_validIntStr = forAll genIntStr $
-  \x -> (x =/= "") .&&. (f x >= 0 .&&. f x <= 4294967295)
-  where f :: String -> Int
-        f = read
+  \x -> classify (isNum x) "number" $
+        (x =/= "") .&&. (asInt x >= 0 .&&. asInt x <= upperInt)
+--------------------------------------------------------------------------------
+-- | check if generated "bad" `Int` string is valid.
+prop_validBadIntStr :: Property
+prop_validBadIntStr = forAll genBadIntStr $
+  \x -> classify (x == "") "empty" $
+        classify (isNum x) "number" $
+        classify (notNum x) "non-number" $
+        check x
+  where check :: String -> Property
+        check x | x == ""   = property True
+                | isNum x   = asInt x < 0 .||. asInt x > upperInt
+                | notNum x  = property True
+                | otherwise = property False
 --------------------------------------------------------------------------------
 -- | properties.
 --------------------------------------------------------------------------------
 -- | check `Int` string -> "binary` string.
 prop_intStrToBinStr :: Property
 prop_intStrToBinStr = forAll genIntStr $
-  \x -> classify (x == "0") "0" $
-        classify (x == "4294967295") "2^32 - 1" $
+  \x -> classify (isNum x) "number" $
+        classify (x == "0") "0" $
+        classify (x == show upperInt) "2^32 - 1" $
         case intStrToBinStr x of
             Left _    -> property False
             Right bin -> check x bin
@@ -79,6 +131,16 @@ prop_intStrToBinStr = forAll genIntStr $
                 r2 :: [(Int, String)] = readInt 2 f g bin
                 f  :: (Char -> Bool)  = (`elem` ['0', '1'])
                 g  :: (Char -> Int)   = Data.Char.digitToInt
+--------------------------------------------------------------------------------
+-- | check "bad" `Int` string -> "binary" string.
+prop_badIntStrToBinStr :: Property
+prop_badIntStrToBinStr = forAll genBadIntStr $
+  \x -> classify (x == "") "empty" $
+        classify (notNum x) "non-number" $
+        classify (isNum x) "number" $
+        case intStrToBinStr x of
+            Left _  -> property True
+            Right _ -> property False
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 -- | run `quickcheck` on all properties automatically.
